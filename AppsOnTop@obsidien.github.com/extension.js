c8f4d6e78602db2c076d22f4733c29e733f5624d
A. B <arnaud.bonatti@gmail.com>
2012-10-02 23:21:27 +0200

    v. 2 is licensing-clear

---
+++ AppsOnTop@obsidien.github.com/extension.js
@@ -0,0 +1,43 @@
+//GNOME Shell extension. Moves the Applications button at the top of the Dash.
+//Copyright (C) 2012 Arnaud Bonatti <arnaud.bonatti@gmail.com>
+
+//This program is free software; you can redistribute it and/or
+//modify it under the terms of the GNU General Public License
+//as published by the Free Software Foundation; either version 2
+//of the License, or (at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, write to the Free Software
+//Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+const Config = imports.misc.config
+const Main   = imports.ui.main
+
+function init(metadata) {
+    let current_version = Config.PACKAGE_VERSION.split('.')
+    if (current_version.length != 3 || current_version[0] != 3)
+        throw new Error("Strange version number (extension.js:24).")
+    
+    switch (current_version[1]) {
+        case"5": global.log("Warning of extension [" + metadata.uuid + "]:\n",
+                            "              Development release detected (" +
+                            Config.PACKAGE_VERSION +
+                            "). Loading as a 3.6 release.\n") //eak
+        case"6": break
+        default: throw new Error("Strange version number (extension.js:32).")
+    }
+}
+
+function toggle() {
+    let dash = Main.overview._dash.actor.get_children()[0]
+    let thingmoved = dash.get_children()[0]
+    dash.remove_actor(thingmoved)
+    dash.add_actor(thingmoved)
+}
+function enable()  { toggle() }
+function disable() { toggle() }
--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-AppsOnTop
-=========
+Apps On Top
+===========
 
 Moves the Applications button at the top of the Dash.
+
+Droits d'auteurs & copyright
+============================
+
+'Apps On Top' is libre software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 2 of the License, or newer.
+
+You should have received a copy of the GNU General Public License along with
+this file. If not, see <http://www.gnu.org/licenses/>.
+
+Install AOT
+===========
+
+The best way to install is to use the GNOME extensions' website at adress:
+https://extensions.gnome.org/extension/472/apps-on-top/
+
+If you know what means 'installing from sources' and if you prefer that, you 
+can download it from git: "git clone git://github.com/Obsidien/AppsOnTop.git".
+When that's done, copy the 'AppsOnTop@obsidien.github.com' directory in your
+~/.local/share/gnome-shell/extensions directory, and enable the Apps On Top
+extension, for exemple by 'gnome-tweak-tool' (or by editing the dconf key
+org.gnome.shell.enabled-extensions if you prefer manual things). Then, restart
+your GNOME Shell. Yeah, that's pretty more complicated than the website way.
