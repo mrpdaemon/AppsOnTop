2f4450ed57859955c940f8bb53ad12e6560a8e13
Mark Pariente <markpariente@gmail.com>
2013-05-04 23:48:02 -0700

    Don't error out with GNOME 3.8

--- AppsOnTop@obsidien.github.com/extension.js
+++ AppsOnTop@obsidien.github.com/extension.js
@@ -29,6 +29,7 @@ function init(metadata) {
                             Config.PACKAGE_VERSION +
                             "). Loading as a 3.6 release.\n") //eak
         case"6": break
+        case"8": break
         default: throw new Error("Strange version number (extension.js:32).")
     }
 }
